01c555d32102145ada3ea82018fb4ea32945678a
tac <tacmathys@gmail.com>
2022-05-01 18:47:46 +0200

    struct

--- srcs/main.s
+++ srcs/main.s
@@ -1,7 +1,9 @@
 %define PROG_SIZE _end - main
 %define O_RDWR    2
+%define O_DIRECTORY  0200000
 %define SEEK_END  2
-%define PROT_RDWD 3
+%define PROT_READ 1
+%define PROT_WRITE 2
 %define MAP_SHARED 1
 
 %define SYS_OPEN  2
@@ -10,11 +12,28 @@
 %define SYS_MMAP  9
 %define SYS_MUNMAP 11
 
+struc   Elf64_Ehdr
+    e_ident:     resb 16   ;       /* Magic number and other info */
+    e_type:      resw 1    ;		/* Object file type */
+    e_machine:   resw 1    ;		/* Architecture */
+  	e_version:   resd 1    ;		/* Object file version */
+  	e_entry:     resq 1    ;		/* Entry point virtual address */
+  	e_phoff:     resq 1    ;		/* Program header table file offset */
+  	e_shoff:     resq 1    ;		/* Section header table file offset */
+  	e_flags:     resd 1    ;		/* Processor-specific flags */
+    e_ehsize:    resw 1    ;		/* ELF header size in bytes */
+    e_phentsize: resw 1    ;		/* Program header table entry size */
+    e_phnum:     resw 1    ;		/* Program header table entry count */
+    e_shentsize: resw 1    ;		/* Section header table entry size */
+    e_shnum:     resw 1    ;		/* Section header table entry count */
+    e_shstrndx:  resw 1    ;		/* Section header string table index */
+endstruc
+
 
 section .data
 
 str:
-    db "entry = %d", 10, 0
+    db "nb phdr = %d", 10, 0
 
 section .text
 
@@ -43,31 +62,37 @@ main:
     mov rax, SYS_OPEN
     syscall              ; open(filemane, O_RDWR)
     cmp rax, 0           ; if (fd < 0) return ;
-    JS exit
+    js exit
     mov r12, rax
     mov rdi, r12
     mov rsi, 0
     mov rdx, SEEK_END
     mov rax, SYS_LSEEK
     syscall             ; lseek(fd, 0, SEEK_END)
+    cmp rax, 0          ; if (!size) return ;
+    jz exit
     mov r13, rax
     mov rdi, 0
     mov rsi, r13
-    mov rdx, PROT_RDWD
-    mov r10, MAP_SHARED
+    mov rdx, PROT_READ | PROT_WRITE
+    mov r10, MAP_SHARED ; r10 est le 4 ieme argument car rcx et r11 sont détruit par le kernel
     mov r8, r12
     mov r9, 0
     mov rax, SYS_MMAP
     syscall             ; mmap(0, fileSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0)
+    cmp rax, 0          ; if (!ptr) return ;
+    jz exit
     mov r14, rax
     
     
     lea rdi, [rel str]
-    mov rsi, r14
+    xor rax, rax
+    mov ax, [r14 + e_phnum]
+    mov rsi, rax
     call printf
 
 
-    close_all:
+close_all:
     mov rdi, r14
     mov rsi, r13
     mov rax, SYS_MUNMAP 
@@ -75,7 +100,7 @@ main:
     mov rdi, r12
     mov rax, SYS_CLOSE  
     syscall             ; close(fd)
-    exit:
+exit:
     leave
     ret
     
