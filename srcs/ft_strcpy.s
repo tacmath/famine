1c857998231e47210f0d5648860d24c9777341e4
matheme42 <maxence.theme@gmail.com>
2022-05-14 00:21:20 +0200

    short strcpy

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -1,11 +1,10 @@
 
 ft_strcpy:
-	mov rdx, -1
-	str_cpy_loop:
-	inc rdx
-	mov al, BYTE [rsi + rdx]
-	mov BYTE [rdi + rdx], al
-	cmp al, 0
-	jne str_cpy_loop
-	mov rax, rdi
+	mov rcx, -1
+	loop:
+	inc rcx
+	cmp BYTE [rsi + rcx], 0
+	jne loop
+	inc rcx
+	rep movsb
 	ret
